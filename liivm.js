15542b76c169d8f1e21a944ee3276610cdaeaac1
hansu35 <hansu@duck.com>
2024-01-27 17:12:38 +0900

    Update liivm.js

--- liivm.js
+++ liivm.js
@@ -3,19 +3,27 @@
 // icon-color: deep-blue; icon-glyph: phone-volume;
 
 const version = 2
-const version2 = "1.0.1"
+const version2 = "1.0.0"
 
-fm = FileManager.local()
+// fm = FileManager.local()
+fm = FileManager.iCloud()
 
 // 스크립트 자동업데이트 
 const serverVersionCheckUrl = "https://raw.githubusercontent.com/hansu35/scriptable_liivm/main/version"
 const serverContentUrl = "https://raw.githubusercontent.com/hansu35/scriptable_liivm/main/liivm.js"
 var minVer = parseInt(await new Request(serverVersionCheckUrl).loadString())
+console.log(minVer)
 if(version < minVer){
 	var code = await new Request(serverContentUrl).loadString()
+	console.log('업데이트')
+	console.log(Script.name())
+	console.log(fm.documentsDirectory())
+	
 	fm.writeString(fm.joinPath(fm.documentsDirectory(), Script.name() + ".js"), code)
 	return 0
 }
+
+
 
 
 
